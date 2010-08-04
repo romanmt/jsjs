cf742760b2df14e8b2d39f0462cad12b3b904f59
romanmt <romanmt@gmail.com>
2010-08-04 15:30:36 -0400

    added more styling. color the words jersey shore pink

--- public/js/node_client.js
+++ public/js/node_client.js
@@ -6,10 +6,16 @@
  
     socket.addEvent('message', function(message) {
       var tweet = JSON.parse(message)
-      $messages.prepend("<div class='message'><span class='username'>@" + tweet.name + ":</span> " + tweet.text + "</div>");
+      $messages.prepend("<div class='message'><span class='username'>@" + tweet.name + ":</span> " + spanJerseyShore(tweet.text) + "</div>");
       $('.message').first().slideDown('slow');
     });
- } 
+  } 
+
+  var spanJerseyShore = function(message) {
+    var parseTweet = /^(.+)(Jersey Shore|jersey shore)(.+)$/;
+    result = parseTweet.exec(message);
+    return result[1] + "<span class='jersey-shore'>" + result[2] + "</span>" + result[3];
+  }
 
   window.IoClient = IoClient;
 }) ();
